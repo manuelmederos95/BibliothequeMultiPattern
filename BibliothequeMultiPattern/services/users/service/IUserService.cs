5555af9a568f0359fb57761ade7640fe1058ec14
JuanWick <jm.anghileri@wanadoo.fr>
2018-06-26 18:13:20 +0200

    [ADD] Controller_connection

---
+++ BibliothequeMultiPattern/services/users/service/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliothequeMultiPattern.services.users
+{
+    interface IUserService
+    {
+        IUser Connect(string login, string motDePasse);
+
+        bool Add(IUser user);
+
+        bool Remove(string login);
+    }
+}
